f9c4bf280e85012c81db5b1ca9aabc0bc9d77ee5
NadaO <lona_hotmail.com>
2019-09-23 17:55:41 +0300

    creat game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
 
-console.log("Up and running!");
-var card1 = "queen";
-var card2 = "queen";
-var card3 = "king";
-var card4 = "king";
-console.log("User flipped " + card1);
+// console.log("Up and running!");
+
+ let cards = ["queen" , "queen", " king" , " king"];
+ let cardsInPlay = [];
+ var cardOne = cards[0];
+ var cardTwo = cards[2];
+ cardsInPlay.push(cardOne);
+  cardsInPlay.push(cardTwo);
+ console.log("User flipped " + cardsInPlay[0]);
+ console.log("User flipped " + cardsInPlay[1]);
+ if(cardsInPlay.length===2);
+  {
+  	if (cardsInPlay[0]!== cardsInPlay[1])
+  		alert(" no match");
+  }
