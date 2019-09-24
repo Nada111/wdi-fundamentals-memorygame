de0624964604f435f562e970ec195e4612b41c17
NadaO <lona_hotmail.com>
2019-09-24 21:36:57 +0300

    creat some logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,14 +3,23 @@
 
  let cards = ["queen" , "queen", " king" , " king"];
  let cardsInPlay = [];
- var cardOne = cards[0];
- var cardTwo = cards[2];
- cardsInPlay.push(cardOne);
-  cardsInPlay.push(cardTwo);
- console.log("User flipped " + cardsInPlay[0]);
- console.log("User flipped " + cardsInPlay[1]);
+ function checkForMatch(){
+ 	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+ }
+
+ function filpCard( cardId){
+ 	console.log( "User flipped " + cards[cardId]);
+ 	cardsInPlay.push(cards[cardId]);
+
  if(cardsInPlay.length===2);
   {
-  	if (cardsInPlay[0]!== cardsInPlay[1])
-  		alert(" no match");
+     checkForMatch();
+  
   }
+}
+filpCard(0);
+filpCard(2);
