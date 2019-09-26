796cd0a6eac084434067e3bb0fce1ac9ab1898fd
NadaO <lona_hotmail.com>
2019-09-26 09:13:21 +0300

    creat game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,15 +8,18 @@
  	if (cardsInPlay[0]=== cardsInPlay[1]) {
   console.log("You found a match!");
 } else {
-  alertß("Sorry, try again.");
+  ale("Sorry, try again.");
 }
  }
 
- function filpCard(cardId){
+ function filpCard(){
+ 	 var cardId = this.getAttribute('data-id');
  	cardsInPlay.push(cards[cardId]);
  	console.log( "User flipped " + cards[cardId].rank);
  	console.log( cards[cardId].suit);
  	console.log( cards[cardId].cardImage);
+ 	this.setAttribute(cards[cardId].cardImage)
+
 
  if(cardsInPlay.length===2)
   {
@@ -24,5 +27,16 @@
   
   }
 }
-filpCard(0);
-filpCard(2);
+function createBoard()
+ {
+ 	for (var i = 0; i < cards.length; i++) {
+
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('img', "images/back.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard());
+    document.getElementById("mgame-board").appendChild(cardElement);  
+}
+
+ } 
+createBoard();
